4608b68295d005c5ee547200297e3861b4597a7a
Ahmed Saad <ahmed.saad.kader@gmail.com>
2022-04-17 13:11:38 +0200

    random computer opponent

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const Gameboard = (() => {
     let gameOver = false
     let turn = 'X'
     let clickBoardCounter = 0
+    let winner, cells
     const mainBoard = document.querySelector('.board')
     const drawBoard = ()=>{
         for (let index = 0; index < 9; index++) {
@@ -18,8 +19,23 @@ const Gameboard = (() => {
             return turn
         } else {return}
     }
-    const win = () => {
-        const cells = document.querySelectorAll('.cell')
+
+    const randomCompMove = (Player1, Player2) => {
+        cells = document.querySelectorAll('.cell')
+        let emptyCells = []
+        cells.forEach(cell => {
+            if (cell.innerHTML == ''){
+                emptyCells.push(cell)
+            }
+        })
+        const randomMove =  emptyCells[Math.floor(Math.random() * emptyCells.length)]
+        if (randomMove){
+                randomMove.innerHTML = 'O'
+            }
+    }
+
+    const win = (Player1, Player2) => {
+        cells = document.querySelectorAll('.cell')
         if (
         (cells[0].innerHTML == cells[1].innerHTML && cells[1].innerHTML == cells[2].innerHTML && cells[0].innerHTML != '')||
         (cells[3].innerHTML == cells[4].innerHTML && cells[4].innerHTML == cells[5].innerHTML && cells[3].innerHTML != '')||
@@ -30,14 +46,18 @@ const Gameboard = (() => {
         (cells[0].innerHTML == cells[4].innerHTML && cells[4].innerHTML == cells[8].innerHTML && cells[0].innerHTML != '')||
         (cells[2].innerHTML == cells[4].innerHTML && cells[4].innerHTML == cells[6].innerHTML && cells[2].innerHTML != '')){
             gameOver = true
-            document.querySelector('#winner-announce').innerHTML = `${turn}`
+            Player1.winRound(turn)
+            Player2.winRound(turn)
+            document.querySelector('#winner-announce').innerHTML = `<span>${turn}</span><span>&nbspwins</span>`
             document.querySelector('dialog').open = true
+            return 'OK'
         } else if (clickBoardCounter == 9) {
-            document.querySelector('#winner-announce').innerHTML = 'tie'
+            document.querySelector('#winner-announce').innerHTML = '<span>Tie</span>'
             document.querySelector('dialog').open = true
+            return
         }
     }
-    const renderPlay = function(...playersLetters) {
+    const renderPlay = function(Player1, Player2, ...PlayersTypes) {
         drawBoard()
         gameboard.forEach((cell) => {
                 cell.addEventListener('click', () => {
@@ -46,7 +66,15 @@ const Gameboard = (() => {
                             play()
                             cell.innerHTML = turn
                             clickBoardCounter ++
-                            win()
+                            if (win(Player1, Player2) == 'OK') {
+                                return
+                            }
+                            if (PlayersTypes.includes('AI')){
+                                play()
+                                randomCompMove()
+                                win(Player1, Player2)
+                                clickBoardCounter ++
+                            }
                         }
                     }
             })
@@ -62,32 +90,33 @@ const Gameboard = (() => {
         }
     }
     return {
-        renderPlay, turn, resetBoard
+        renderPlay, resetBoard, turn, winner
     }
 
 })();
 
 
-const player = (name, letter) => {
+const player = (name, letter, type) => {
     let score = 0;
     let playerScoreP
     const updateDisplay = (playerDivID) => {
         const playerNameP = document.createElement('p')
-        playerNameP.innerHTML = `Player Name: ${name}`
+        playerNameP.innerHTML = `<span>Player Name: </span> <h4 id="h4-margin-0">${name}</h4>`
+        const humanOrAI = document.createElement('h4')
+        humanOrAI.innerHTML = type
         playerScoreP = document.createElement('p')
-        playerScoreP.innerHTML = `Score: ${score}`
+        playerScoreP.innerHTML = `<span>Score: </span><h4>${score}</h4>`
         playerDivID.appendChild(playerNameP)
         playerDivID.appendChild(playerScoreP)
+        playerDivID.appendChild(humanOrAI)
     };
-    const winRound = () => {
-        console.log(document.querySelector('#winner-announce').innerHTML)
-        if (letter == document.querySelector('#winner-announce').innerHTML){
+    const winRound = (turn) => {
+        if (letter == turn){
             score += 1
-            console.log(`${name}: ${score}`)
-            playerScoreP.innerHTML = `Score: ${score}`
+            playerScoreP.innerHTML = `<span>Score: </span><h4>${score}</h4>`
         }
     };
-    return {name, letter, updateDisplay, winRound}
+    return {name, letter,type, updateDisplay, winRound}
 }
 
 
@@ -101,11 +130,11 @@ let Player1, Player2
 player1form.addEventListener('submit', (e)=> {
     e.preventDefault()
     const player1Name = player1form.elements.name.value
-    // const player1Letter = player1form.elements.letter.value
-    Player1 = player(player1Name, 'X')
+    const player1Type = player1form.elements.playerType.value
+    Player1 = player(player1Name, 'X', player1Type)
     player1form.style.display = 'none'
     Player1.updateDisplay(player1Display);
-    if (player2form.elements.name.value !== ''){
+    if (Player2 !== undefined){
         playGame.classList.remove('hidden')
     }
 })
@@ -113,11 +142,11 @@ player1form.addEventListener('submit', (e)=> {
 player2form.addEventListener('submit', (e)=> {
     e.preventDefault()
     const player2Name = player2form.elements.name.value
-    // const player2Letter = player2form.elements.letter.value
-    Player2 = player(player2Name, 'O')
+    const player2Type = player2form.elements.playerType.value
+    Player2 = player(player2Name, 'O', player2Type)
     player2form.style.display = 'none'
     Player2.updateDisplay(player2Display);
-    if (player1form.elements.name.value !== ''){
+    if (Player1 !== undefined){
         playGame.classList.remove('hidden')
     }
 
@@ -127,18 +156,14 @@ const resetButton = document.getElementById('play-again')
 const newGame = document.getElementById('new-game')
 
 playGame.addEventListener('click', () =>{
-    Gameboard.renderPlay()
+    Gameboard.renderPlay(Player1, Player2, Player1.type, Player2.type)
+    playGame.classList.add('hidden')
 })
 resetButton.addEventListener('click', ()=> {
-    Player1.winRound(Gameboard.turn)
-    Player2.winRound(Gameboard.turn)
     Gameboard.resetBoard()
     document.querySelector('dialog').open = false
-    Gameboard.renderPlay()
-
+    Gameboard.renderPlay(Player1, Player2,Player1.type, Player2.type)
 })
 newGame.addEventListener('click', ()=> {
     location.reload()
 })
-
-console.log(player1form.elements.name.value == '')
